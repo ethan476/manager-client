7497f0cb00d91350ec1a436797ce55ad82b1ca2b
Niles Rogoff <nilesrogoff@gmail.com>
2016-05-03 19:16:28 -0400

    Made temp more useful

--- client/modules/temp.py
+++ client/modules/temp.py
@@ -5,28 +5,21 @@ from enum import Enum
 
 class module(Module):
 	
-	def trigger_sleep_8(self, event):
+	def overheat_checker(self, event):
 		while True:
 			time.sleep(8);
-			event(self, 10);
-
-	def trigger_sleep_10(self, event):
-		while True:
-			time.sleep(10);
-			event(self, 11);
+			if get_value() > 50:
+				event(self, 10);
 
 	def trigger_10_called(self):
-		return 99
-	def trigger_11_called(self):
-		return 120
+		return ["OVERHEAT WARNING", get_value()]
 
 	self.provides = "temp";
 
-	self.listeners = [trigger_sleep_8, trigger_sleep_10];
+	self.listeners = [overheat_checker];
 
 	def __init__(self, register, triggers):
 		register(self, 10, trigger_10_called)
-		register(self, 11, trigger_11_called)
 		register(self, triggers.STARTUP)
 
 	def get_value(self, server_request = None):
