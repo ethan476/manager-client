220ee7fa8fd33e59ae2b62a7dcf78bbdf020430c
Ethan <mcdasethan2@gmail.com>
2016-05-03 19:39:40 -0400

    Fixed triggers in temp.py

--- client/modules/temp.py
+++ client/modules/temp.py
@@ -2,7 +2,7 @@ import random, sys;
 
 from enum import Enum
 
-class CustomTriggers(Enum):
+class custom_triggers(Enum):
 	OVERHEAT = 1024;
 
 class module():
@@ -17,13 +17,13 @@ class module():
 		while True:
 			time.sleep(8);
 			if get_value() > 50:
-				event(self, CustomTriggers.OVERHEAT);
+				event(self, custom_triggers.OVERHEAT);
 
 	def trigger_10_called(self):
 		return ["OVERHEAT WARNING", get_value()]
 
 	def __init__(self, register, triggers):
-		register(self, CustomTriggers.OVERHEAT, trigger_10_called);
+		register(self, custom_triggers.OVERHEAT, trigger_10_called);
 		register(self, triggers.STARTUP);
 
 	def get_value(self, server_request = None):
