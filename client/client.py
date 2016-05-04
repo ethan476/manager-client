17951c186103d00c9f20bc16fb104f70c9b8a437
Niles Rogoff <nilesrogoff@gmail.com>
2016-05-03 20:09:35 -0400

    Fixed error where server requests could be misinterpereted as triggers

--- client/client.py
+++ client/client.py
@@ -4,27 +4,32 @@ from .triggers import Triggers;
 
 class Client():
 
-	def event(self, module, server_request):
+	def event(self, module, server_request, trigger = True):
 		for q in listener_queues:
-			q.put([module.provides, server_request]);
+			q.put([module.provides, server_request, trigger]);
 
 	def listener(self, q, trigger, p, module, trigger_done):
 		while True:
 			temp = q.get();
-			if temp == [module.provides, trigger]:
+			if temp == [module.provides, trigger, True]:
 				p.put([module, True, trigger_done(trigger)]);
-			elif temp[0] == module:
-				p.put([module, False, module.get_value(temp[1])]);
-
+	def server_request_listener(self, q, p, module):
+		while True:
+			temp = q.get();
+			if temp[0] == module.provides and not temp[2]:
+				p.put([module, False, module.get_value(temp[1])])
 	def global_queue_listener_function(self, p):
 		while True:
 			self.server_send(*p.get());
 
-	def register(self, module, trigger, trigger_done = False):
+	def register(self, module, trigger, trigger_done = False, server_request = False):
 		if not trigger_done:
 			trigger_done = module.trigger_called
 		q = queue.Queue()
-		h = threading.Thread(target = self.listener, args = (q, trigger, global_event_queue, module, trigger_done));
+		if server_request:
+			h = threading.Thread(target = self.server_request_listener, args = (q, global_event_queue, module));
+		else:
+			h = threading.Thread(target = self.listener, args = (q, trigger, global_event_queue, module, trigger_done));
 		h.start();
 		listeners.append(h);
 		listener_queues.append(q);
@@ -40,6 +45,7 @@ class Client():
 		global_queue_listener.start()
 		for module in modules:
 			self.event(module, Triggers.STARTUP)
+			self.register(module, False, False, True)
 			for thread in module.listeners:
 				h = threading.Thread(target = thread, args = (self.event,))
 				h.start()
